d8a69d04eec0bc368e6734e5a08ba5715389801d
SzabotinErik <szabotine.12d@acsjszki.hu>
2022-01-28 13:11:11 +0100

    2.f

--- 2022-01-28_Opening-weekend/Class1.cs
+++ 2022-01-28_Opening-weekend/Class1.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace _2022_01_28_Opening_weekend
 {
-    class Class1
+    class Film
     {
+        public string eredeticim { get; set; }
+        public string magyarcim { get; set; }
+        public string bemutato { get; set; }
+        public string forgalmazo { get; set; }
+        public int bevel { get; set; }
+        public int latogato { get; set; }
 
+        public Film(string sor)
+        {
+            string[] t = sor.Split(';');
+            eredeticim = t[0];
+            magyarcim = t[1];
+            bemutato = t[2];
+            forgalmazo =[3];
+            bevel = int.Parse(t[4]);
+            latogato = int.Parse(t[5]);
+        }
     }
 }
