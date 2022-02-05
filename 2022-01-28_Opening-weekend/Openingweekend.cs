786d1bbca3de018b7554b66836577ac71f958fd4
SzabotinErik <szabotine.12d@acsjszki.hu>
2022-02-05 09:09:52 +0100

    4.feladat

--- 2022-01-28_Opening-weekend/Openingweekend.cs
+++ 2022-01-28_Opening-weekend/Openingweekend.cs
@@ -17,6 +17,41 @@ namespace _2022_01_28_Opening_weekend
                 filmek.Add(new Film(sor));
             }
             Console.WriteLine($"3.feladat: filmek száma: {filmek.Count} db");
+            
+            int osszeg;
+            foreach (var i in filmek)
+	        {
+                if (i.forgalmazo == "UIP" && i.bemutato == "2016.12.01")
+	            {
+                    osszeg=osszeg+i.bevel;
+	            }
+                if else(i.forgalmazo == "UIP" && i.bemutato == "2016.12.02")
+	            {
+                    osszeg=osszeg+i.bevel;
+	            }
+                if else(i.forgalmazo == "UIP" && i.bemutato == "2016.12.03")
+	            {
+                    osszeg=osszeg+i.bevel;
+	            }
+                if else(i.forgalmazo == "UIP" && i.bemutato == "2016.12.04")
+	            {
+                    osszeg=osszeg+i.bevel;
+	            }
+                if else(i.forgalmazo == "UIP" && i.bemutato == "2016.12.05")
+	            {
+                    osszeg=osszeg+i.bevel;
+	            }
+                if else(i.forgalmazo == "UIP" && i.bemutato == "2016.12.06")
+	            {
+                    osszeg=osszeg+i.bevel;
+	            }
+                if else(i.forgalmazo == "UIP" && i.bemutato == "2016.12.07")
+	            {
+                    osszeg=osszeg+i.bevel;
+	            }
+	        }
+            Console.WriteLine($"4.feladat:UIP Duna Film forgalmazó bevetele{osszeg} ");
+
             Console.ReadKey();
         }
     }
